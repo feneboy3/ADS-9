5b1f009b479490aa0b38a3e0dd6678e13beb232c
feneboy3 <dankala2007@gmail.com>
2026-06-02 13:58:49 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,61 +1,60 @@
 // Copyright 2022 NNTU-CS
-#include "tree.h"
 #include <iostream>
 #include <vector>
 #include <chrono>
-
-using namespace std;
-using namespace std::chrono;
+#include "tree.h"
 
 int main() {
-    vector<char> in = { '1', '2', '3' };
+    std::vector<char> in = {'1', '2', '3'};
     Tree tree(in);
 
-    vector<vector<char>> all = getAllPerms(tree);
+    std::vector<std::vector<char>> all = getAllPerms(tree);
     for (size_t i = 0; i < all.size(); ++i) {
         for (size_t j = 0; j < all[i].size(); ++j) {
-            cout << all[i][j];
+            std::cout << all[i][j];
         }
-        cout << " ";
+        std::cout << " ";
     }
-    cout << "\n\n";
+    std::cout << "\n\n";
 
     int search_num = 4;
-    vector<char> p1 = getPerm1(tree, search_num);
-    for (char c : p1) cout << c;
-    cout << endl;
+    std::vector<char> p1 = getPerm1(tree, search_num);
+    for (char c : p1) std::cout << c;
+    std::cout << "\n";
 
-    vector<char> p2 = getPerm2(tree, search_num);
-    for (char c : p2) cout << c;
-    cout << "\n\n";
+    std::vector<char> p2 = getPerm2(tree, search_num);
+    for (char c : p2) std::cout << c;
+    std::cout << "\n\n";
 
-    vector<int> sizes = { 3, 4, 5, 6, 7, 8 };
-
+    std::vector<int> sizes = {3, 4, 5, 6, 7, 8};
+    
     for (int n : sizes) {
-        vector<char> test_in;
+        std::vector<char> test_in;
         for (int i = 0; i < n; ++i) {
             test_in.push_back('A' + i);
         }
-
+        
         Tree test_tree(test_in);
-
-        int test_num = 1;
+        
+        int test_num = 1; 
         for (int i = 1; i <= n; ++i) test_num *= i;
-        test_num = test_num - 1;
+        test_num = test_num - 1; 
 
-        auto start1 = high_resolution_clock::now();
+        auto start1 = std::chrono::high_resolution_clock::now();
         getPerm1(test_tree, test_num);
-        auto stop1 = high_resolution_clock::now();
-        auto dur1 = duration_cast<nanoseconds>(stop1 - start1).count();
-
-        auto start2 = high_resolution_clock::now();
+        auto stop1 = std::chrono::high_resolution_clock::now();
+        auto dur1 = std::chrono::duration_cast<std::chrono::nanoseconds>(
+            stop1 - start1).count();
+        
+        auto start2 = std::chrono::high_resolution_clock::now();
         getPerm2(test_tree, test_num);
-        auto stop2 = high_resolution_clock::now();
-        auto dur2 = duration_cast<nanoseconds>(stop2 - start2).count();
-
-        cout << "N = " << n << "\t|"
-            << " getPerm1: " << dur1 << " ns \t|"
-            << " getPerm2: " << dur2 << " ns" << endl;
+        auto stop2 = std::chrono::high_resolution_clock::now();
+        auto dur2 = std::chrono::duration_cast<std::chrono::nanoseconds>(
+            stop2 - start2).count();
+        
+        std::cout << "N = " << n << "\t|" 
+             << " getPerm1: " << dur1 << " ns \t|" 
+             << " getPerm2: " << dur2 << " ns\n";
     }
 
     return 0;
